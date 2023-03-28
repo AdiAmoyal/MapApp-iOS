04faada181c9a9a40ed3839bbec81b94bf3b5069
Adi Amoyal <adi.amoyal02@gmail.com>
2023-03-28 19:13:04 +0300

    Creating animated menu of locations list

--- MapApp/ViewModels/LocationViewModel.swift
+++ MapApp/ViewModels/LocationViewModel.swift
@@ -22,6 +22,8 @@ class LocationViewModel: ObservableObject {
     @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
     let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
+    @Published var showLocationsList: Bool = false
+    
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
@@ -33,6 +35,19 @@ class LocationViewModel: ObservableObject {
     private func updateMapRegion(location: Location) {
         withAnimation(.easeInOut) {
             mapRegion = MKCoordinateRegion(center: location.coordinates, span: mapSpan)
+        }
+    }
+    
+    func toggleLocationsList() {
+        withAnimation(.easeInOut) {
+            showLocationsList = !showLocationsList
+        }
+    }
+    
+    func showNextLocation(location: Location) {
+        withAnimation(.easeInOut) {
+            mapLocation = location
+            showLocationsList = false
         }
     }
 }
