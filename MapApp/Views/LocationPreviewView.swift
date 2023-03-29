83e338e1e5496a3b511598ada99d4cd7ab77742b
Adi Amoyal <adi.amoyal02@gmail.com>
2023-03-29 22:55:43 +0300

    Create Location Preview Card

---
+++ MapApp/Views/LocationPreviewView.swift
@@ -0,0 +1,90 @@
+//
+//  LocationPreviewView.swift
+//  MapApp
+//
+//  Created by Adi Amoyal on 29/03/2023.
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    
+    @EnvironmentObject private var vm: LocationViewModel
+    let location: Location
+    
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 0) {
+            VStack(alignment: .leading, spacing: 16) {
+                imageSection
+                titleSection
+            }
+            
+            VStack(spacing: 8) {
+                learnMoreBtn
+                nextBtn
+            }
+        }
+        .padding(20)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.ultraThinMaterial)
+                .offset(y: 65)
+        )
+        .cornerRadius(10)
+    }
+}
+
+extension LocationPreviewView {
+    
+    private var imageSection: some View {
+        ZStack {
+            if let imageName = location.imageNames.first {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+            }
+        }
+        .padding(6)
+        .background(Color.white)
+        .cornerRadius(10)
+    }
+    
+    private var titleSection: some View {
+        VStack(alignment: .leading) {
+            Text(location.name)
+                .font(.title2)
+                .fontWeight(.bold)
+            
+            Text(location.cityName)
+                .font(.subheadline)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var learnMoreBtn: some View {
+        Button(action: {  }) {
+            Text("Learn more")
+                .font(.headline)
+                .frame(width: 125, height: 30)
+        }
+        .buttonStyle(.borderedProminent)
+    }
+    
+    private var nextBtn: some View {
+        Button(action: vm.nextButtonPressed) {
+            Text("Next")
+                .font(.headline)
+                .frame(width: 125, height: 30)
+        }
+        .buttonStyle(.bordered)
+    }
+}
+
+struct LocationPreviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationPreviewView(location: LocationsDataService.locations.first!)
+    }
+}
+
